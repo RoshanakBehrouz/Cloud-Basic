be45b2479f0f5d4f1132ba5881009ea28046229a
Roshanak Behrouz <131586234+RoshanakBehrouz@users.noreply.github.com>
2024-03-19 14:39:43 +0100

    Delete Locust/create_users.sh

--- Locust/create_users.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loop to create users on only one Nextcloud instance
-for user in $(seq 0 29); do
-    # Create user using Docker exec with the specified password
-    docker exec -e OC_PASS=14070929@R --user www-data nextcloud_app1_1 /var/www/html/occ user:add --password-from-env "User${user}"
-done
-
